71d4928d5d40343bab0e86453dcbbf84769fd971
ChiefWoods <chii.yuen@hotmail.com>
2024-10-02 13:08:56 +0800

    Init Anchor project

---
+++ programs/ruma-anchor/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("7CNQHU6f1cxCUjkhqCMPN8aEFofREEsVUFsCA72gV6fj");
+
+#[program]
+pub mod ruma_anchor {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
