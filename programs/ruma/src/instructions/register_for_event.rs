114bed8fc75713748997fa74aea28aea3f925d7c
ChiefWoods <chii.yuen@hotmail.com>
2024-10-31 23:13:05 +0800

    Complete register_for_event

--- programs/ruma/src/instructions/register_for_event.rs
+++ programs/ruma/src/instructions/register_for_event.rs
@@ -1,8 +1,39 @@
+use crate::constants::*;
+use crate::error::*;
+use crate::state::*;
 use anchor_lang::prelude::*;
 
-pub fn register_for_event(ctx: Context<RegisterForEvent>) -> Result<()> {
+pub fn register_for_event(ctx: Context<RegisterForEvent>, name: String) -> Result<()> {
+    require!(name.len() <= 128, RumaError::EventNameTooLong);
+
+    let event = &mut ctx.accounts.event;
+
+    event.attendees.push(Attendee {
+        user: (*ctx.accounts.attendee).clone(),
+        status: AttendeeStatus::Pending,
+    });
+
     Ok(())
 }
 
 #[derive(Accounts)]
-pub struct RegisterForEvent {}
+#[instruction(name: String)]
+pub struct RegisterForEvent<'info> {
+    #[account(
+        mut,
+        address = RUMA_WALLET
+    )]
+    pub payer: Signer<'info>,
+    pub organizer: Account<'info, User>,
+    pub attendee: Account<'info, User>,
+    #[account(
+        mut,
+        seeds = [EVENT_SEED.as_bytes(), organizer.key().as_ref(), name.as_bytes()],
+        bump = event.bump,
+        realloc = event.to_account_info().data_len() + Attendee::min_space(),
+        realloc::payer = payer,
+        realloc::zero = false,
+    )]
+    pub event: Account<'info, Event>,
+    pub system_program: Program<'info, System>,
+}
--- programs/ruma/src/state/attendee.rs
+++ programs/ruma/src/state/attendee.rs
@@ -1,19 +1,29 @@
-use anchor_lang::{prelude::*, Discriminator};
 use crate::state::*;
+use anchor_lang::{prelude::*, Discriminator};
+use num_derive::*;
 
 #[account]
 pub struct Attendee {
     pub user: User,
-    pub qr_hash: String,
+    pub status: AttendeeStatus,
 }
 
 impl Attendee {
     pub fn min_space() -> usize {
         let mut space = Attendee::DISCRIMINATOR.len();
 
-        // user, qr_hash
-        space += User::min_space() + 4;
+        // user, status
+        space += User::min_space() + (1 + 1);
 
         space
     }
+}
+
+#[derive(
+    AnchorSerialize, AnchorDeserialize, FromPrimitive, ToPrimitive, Copy, Clone, PartialEq, Eq,
+)]
+pub enum AttendeeStatus {
+    Pending,
+    Approved,
+    Rejected,
 }
