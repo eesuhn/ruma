ce9d3f3480bd77700c9efed51fe1cdd79eb97877
eesuhn <eason.yihong@gmail.com>
2024-11-05 15:37:56 +0800

    Completed nft creation page

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -7,7 +7,7 @@ import type { AppProps } from "next/app";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-    <div className="flex flex-col min-h-screen  bg-gradient-to-b from-blue-200 to-white bg-fixed">
+    <div className="flex flex-col min-h-screen bg-fixed">
         <Navbar />
         {/* <div className="flex items-center place-content-center justify-items-center flex-1"> */}
             <Component {...pageProps} />
