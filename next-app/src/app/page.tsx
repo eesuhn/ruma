112230bfc45ce1b7de329b1c5bd19a48fbf94558
eesuhn <eason.yihong@gmail.com>
2024-11-30 18:17:03 +0800

    Updated homepage

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,30 +1,44 @@
 import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="flex flex-1 place-content-center items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
-      <main className="mb-10 flex flex-row items-center">
-        <div className="mr-8 flex flex-col">
-          <h1 className="text-[42px] font-bold">Ruma</h1>
-          <p className="font-[family-name:var(--font-geist-mono)] text-[24px]">
-            Start joining events on the chain and get some cool NFTs
+    <div className="flex min-h-screen flex-col">
+      {/* Hero Section */}
+      <section className="container flex flex-col-reverse items-center gap-8 px-24 py-12 md:flex-row md:py-24">
+        <div className="flex flex-col items-center space-y-6 text-center md:w-1/2 md:items-start md:text-left">
+          <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
+            Collect Memories as NFTs
+          </h1>
+          <p className="pl-2 text-xl text-muted-foreground">
+            Join events, connect with like-minded people, and collect exclusive
+            NFTs that prove you were there.
           </p>
-          <a
-            className="mt-4 flex h-10 w-[25%] items-center justify-center gap-2 rounded-full border border-solid border-transparent bg-foreground px-4 text-[24px] text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] sm:h-12 sm:px-5 sm:text-base"
-            href="/events"
-          >
-            <p className="text-[24px]">Events</p>
-          </a>
+          <div className="flex gap-4 pl-2">
+            <Link href="/discover">
+              <Button size="lg" className="h-12 px-8">
+                Discover Events
+              </Button>
+            </Link>
+            <a href="/create-event">
+              <Button variant="outline" size="lg" className="h-12 px-8">
+                Host Event
+              </Button>
+            </a>
+          </div>
         </div>
-        <Image
-          className="rounded-md shadow-lg dark:invert"
-          src={'/sample/landing-hero.jpg'}
-          alt="Landing Image"
-          width={360}
-          height={38}
-          priority
-        />
-      </main>
+        <div className="items-center md:w-1/2">
+          <Image
+            src="/sample/landing-hero.jpg"
+            alt="Event Platform Interface"
+            width={500}
+            height={500}
+            className="rounded-3xl shadow-xl"
+            priority
+          />
+        </div>
+      </section>
     </div>
   );
 }
