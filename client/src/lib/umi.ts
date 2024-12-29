6672c340a175d36de6f740aadae18d0924ee6a60
ChiefWoods <chii.yuen@hotmail.com>
2024-12-29 12:00:56 +0800

    Rename Umi functions

--- client/src/lib/umi.ts
+++ client/src/lib/umi.ts
@@ -2,24 +2,24 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { CONNECTION } from "./constants";
 import { PublicKey } from "@solana/web3.js";
 import { PublicKey as UmiPublicKey } from '@metaplex-foundation/umi';
-import { DigitalAsset, fetchDigitalAsset, fetchMasterEdition, fetchMetadata, findMasterEditionPda, findMetadataPda, MasterEdition, Metadata } from "@metaplex-foundation/mpl-token-metadata";
+import { Edition, fetchEdition, fetchMasterEdition, fetchMetadata, findMasterEditionPda, findMetadataPda, MasterEdition, Metadata } from "@metaplex-foundation/mpl-token-metadata";
 import { fromWeb3JsPublicKey } from "@metaplex-foundation/umi-web3js-adapters";
 
 export const umi = createUmi(CONNECTION.rpcEndpoint, 'confirmed');
 
-export function getMasterEditionPda(
-  masterMintPubkey: PublicKey
+export function getMasterOrPrintedEditionPda(
+  mintPubkey: PublicKey
 ): UmiPublicKey {
   return findMasterEditionPda(umi, {
-    mint: fromWeb3JsPublicKey(masterMintPubkey),
+    mint: fromWeb3JsPublicKey(mintPubkey),
   })[0];
 }
 
 export function getMetadataPda(
-  masterMintPubkey: PublicKey
+  mintPubkey: PublicKey
 ): UmiPublicKey {
   return findMetadataPda(umi, {
-    mint: fromWeb3JsPublicKey(masterMintPubkey),
+    mint: fromWeb3JsPublicKey(mintPubkey),
   })[0];
 }
 
@@ -30,9 +30,9 @@ export async function getMasterEditionAcc(
 }
 
 export async function getEditionAcc(
-  editionMintPubkey: PublicKey
-): Promise<DigitalAsset> {
-  return await fetchDigitalAsset(umi, fromWeb3JsPublicKey(editionMintPubkey));
+  editionPda: UmiPublicKey
+): Promise<Edition> {
+  return await fetchEdition(umi, editionPda);
 }
 
 export async function getMetadataAcc(
